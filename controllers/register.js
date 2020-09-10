caeef3cc33b9bbb8b7aee5173900430e6f748fc7
darrylgit <darrylsmithup@gmail.com>
2020-09-10 10:15:50 -0400

    Updgrade pg and knex

--- controllers/register.js
+++ controllers/register.js
@@ -4,8 +4,9 @@ const handleRegister = (req, res, db, bcrypt) => {
     return res.status(400).json('incorrect form submission');
   }
   const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-      trx.insert({
+  db.transaction(trx => {
+    trx
+      .insert({
         hash: hash,
         email: email
       })
@@ -21,16 +22,16 @@ const handleRegister = (req, res, db, bcrypt) => {
           })
           .then(user => {
             res.json(user[0]);
-          })
+          });
       })
       .then(trx.commit)
-      .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'))
-}
+      .catch(trx.rollback);
+  }).catch(err => {
+    console.log(err.message);
+    res.status(400).json('unable to register');
+  });
+};
 
 module.exports = {
   handleRegister: handleRegister
 };
-
-
