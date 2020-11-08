6c3514242601067ff53003382f309a6603beb323
darrylgit <darrylsmithup@gmail.com>
2020-11-07 22:55:57 -0500

    Add age and pet to users

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-INSERT into users (name, email, entries, joined) values ('Jessie', 'jessie@gmail.com', 5, '2018-01-01');
+INSERT into users (name, email, entries, joined, age, pet) values ('Jessie', 'jessie@gmail.com', 5, '2018-01-01', '26', 'cat');
 INSERT into login (hash, email) values ('$2a$10$v7Y7UNlmwkUjZhz.Rd5wT.NR2BeTNxO9SvVTEgR8HdOq7.3KukUD6', 'jessie@gmail.com');
 
 COMMIT;
--- postgres/tables/users.sql
+++ postgres/tables/users.sql
@@ -5,7 +5,9 @@ CREATE TABLE users (
   name VARCHAR(100),
   email text UNIQUE NOT NULL,
   entries BIGINT DEFAULT 0,
-  joined TIMESTAMP NOT NULL
+  joined TIMESTAMP NOT NULL,
+  age text NOT NULL,
+  pet text NOT NULL
 );
 
 COMMIT;
