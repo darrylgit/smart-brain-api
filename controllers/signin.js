f7f94166306454734618d1b725e7ba0a18fba0d2
darrylgit <darrylsmithup@gmail.com>
2020-11-10 22:58:35 -0500

    Add JWT

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,3 +1,5 @@
+var jwt = require('jsonwebtoken');
+
 const handleSignin = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
@@ -26,12 +28,31 @@ const handleSignin = (db, bcrypt, req, res) => {
 const getAuthTokenId = () => {
   console.log('auth ok');
 };
+
+const signToken = email => {
+  const jwtPayload = { email };
+  return jwt.sign(jwtPayload, 'JWT_SECRET', { expiresIn: '2 days' });
+};
+
+const createSessions = user => {
+  // JWT token, return user data
+  const { email, id } = user;
+  const token = signToken(email);
+
+  return { success: 'true', userId: id, token };
+};
+
 const signinAuthentication = (db, bcrypt) => (req, res) => {
   const { authorization } = req.headers;
   return authorization
     ? getAuthTokenId()
     : handleSignin(db, bcrypt, req, res)
-        .then(data => res.json(data))
+        .then(data => {
+          return data.id && data.email
+            ? createSessions(data)
+            : Promise.reject(data);
+        })
+        .then(session => res.json(session))
         .catch(err => res.status(400).json(err));
 };
 
